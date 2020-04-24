592515c5304ab1384e1eaa927448dc1cf8689a08
Felipe Megale <megale@ibm.com>
2020-04-24 19:42:08 -0300

    solve 1052

---
+++ beginner/1052.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int main()
+{
+    int mo;
+    scanf("%d", &mo);
+    if (mo == 1)
+        printf("January\n");
+    else if (mo == 2)
+        printf("February\n");
+    else if (mo == 3)
+        printf("March\n");
+    else if (mo == 4)
+        printf("April\n");
+    else if (mo == 5)
+        printf("May\n");
+    else if (mo == 6)
+        printf("June\n");
+    else if (mo == 7)
+        printf("July\n");
+    else if (mo == 8)
+        printf("August\n");
+    else if (mo == 9)
+        printf("September\n");
+    else if (mo == 10)
+        printf("October\n");
+    else if (mo == 11)
+        printf("November\n");
+    else if (mo == 12)
+        printf("December\n");
+    return 0;
+}
